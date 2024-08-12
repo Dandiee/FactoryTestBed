101908548c8319cbb8da2b08a2c636105c6e0a12
Daniel_Koczur <daniel_koczur@epam.com>
2024-08-12 20:59:56 +0200

    init

---
+++ FactoryTestBed/Models.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace FactoryTestBed;
+
+public sealed class Data
+{
+    public IReadOnlyCollection<Item> Items { get; set; }
+    public IReadOnlyDictionary<string, Item> ItemsByName { get; set; }
+    public IReadOnlyCollection<Recipe> Recipes { get; set; }
+
+    public Item GetItem(string name) => ItemsByName[name];
+}
+
+[DebuggerDisplay("{DisplayName}")]
+public sealed class Item
+{
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+
+    public IReadOnlyCollection<Recipe> Recipes { get; set; }
+}
+
+[DebuggerDisplay("{DisplayName}")]
+public sealed class Recipe
+{
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    public float Duration { get; set; }
+    public IReadOnlyCollection<ItemRate> Ingredients { get; set; }
+    public IReadOnlyCollection<ItemRate> Products { get; set; }
+}
+
+[DebuggerDisplay("{Item.DisplayName}, Amount={Amount}")]
+public sealed class ItemRate
+{
+    public string Name { get; set; }
+    public float Amount { get; set; }
+    public Item Item { get; set; }
+
+    public ItemRate() { }
+
+    public ItemRate(Item item, float amount)
+    {
+        Name = item.Name;
+        Amount = amount;
+        Item = item;
+    }
+}
---
+++ FactoryTestBed/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using FactoryTestBed;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var data = DataProvider.Get();
+        var availableResources = new ItemRate[]
+        {
+            new (data.GetItem("Desc_Stone_C"), 480),
+            new (data.GetItem("Desc_OreIron_C"), 1440),
+            new (data.GetItem("Desc_OreCopper_C"), 360),
+            new (data.GetItem("Desc_LiquidOil_C"), 1020 * 1000),
+            new (data.GetItem("Desc_Water_C"), 10000000000),
+            new (data.GetItem("Desc_Coal_C"), 960),
+            new (data.GetItem("Desc_OreGold_C"), 240),
+            new (data.GetItem("Desc_RawQuartz_C"), 480),
+            new (data.GetItem("Desc_OreBauxite_C"), 240),
+            // these make things harder
+            // new (data.GetItem("Desc_NitrogenGas_C"), 100000),
+            // new (data.GetItem("Desc_OreUranium_C"), 1000),
+            // new (data.GetItem("Desc_Sulfur_C"), 1000),
+        };
+
+        Test(data, availableResources, "Desc_IronScrew_C", 23040);
+        Test(data, availableResources, "Desc_Rotor_C", 511.99997f);
+        Test(data, availableResources, "Desc_Computer_C", 37.333336f);
+        Test(data, availableResources, "Desc_ComputerSuper_C", 8.372093f);
+    }
+
+    private static float GetBest(Data data, IReadOnlyCollection<ItemRate> availableResources, string itemName, float expectedAmount)
+    {
+        // TODO: improve
+        return 42;
+    }
+
+    private static void Test(Data data, IReadOnlyCollection<ItemRate> availableResources, string itemName, float expectedAmount)
+    {
+        var item = data.GetItem(itemName);
+        var sw = Stopwatch.StartNew();
+        var best = GetBest(data, availableResources, itemName, expectedAmount);
+        Console.WriteLine($"{item.DisplayName} took {sw.ElapsedMilliseconds:N0} ms");
+        Debug.Assert(Math.Abs(best - expectedAmount) < 0.001);
+    }
+}
